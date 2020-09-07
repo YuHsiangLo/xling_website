39e85742edd481ea1f260da94a31744e2901b33c
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-09-06 18:06:57 -0700

    Moved head tag to the beginning

--- demographic_questionnaire.php
+++ demographic_questionnaire.php
@@ -3,75 +3,74 @@ session_start();
 require_once "language/".$_SESSION['lang']."/main.php";
 ?>
 <!DOCTYPE html>
-<html>
-<style>
-    form {
-        /* Just to center the form on the page */
-        margin: 0 auto;
-        width: 40%;
-        /* To see the outline of the form */
-        padding: 1em;
-        border: 1px solid #CCC;
-        border-radius: 1em;
-    }
-
-    form div + div {
-        margin-top: 1em;
-    }
-
-    label {
-        /* To make sure that all labels have the same size and are properly aligned */
-        display: inline-block;
-        width: 30%;
-        text-align: right;
-    }
-
-    input, textarea {
-        /* To make sure that all text fields have the same font settings
-           By default, textareas have a monospace font */
-        font: 1em sans-serif;
-
-        /* To give the same size to all text fields */
-        width: 65%;
-        box-sizing: border-box;
-
-        /* To harmonize the look & feel of text field border */
-        border: 1px solid #999;
-    }
-
-    input:focus, textarea:focus {
-        /* To give a little highlight on active elements */
-        border-color: #000;
-    }
-
-    textarea {
-        /* To properly align multiline text fields with their labels */
-        vertical-align: top;
-
-        /* To give enough room to type some text */
-        height: 5em;
-    }
-
-    .button {
-        /* To position the buttons to the same position of the text fields */
-        padding-left: 90px; /* same size as the label elements */
-    }
-
-    button {
-        /* This extra margin represent roughly the same space as the space
-           between the labels and their text fields */
-        margin-left: .5em;
-    }
-
-</style>
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Demographic questionnaire</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
     <script src="/bootstrap/js/bootstrap.min.js"></script>
     <script src="/bootstrap/js/jquery.min.js"></script>
+    <style>
+        form {
+            /* Just to center the form on the page */
+            margin: 0 auto;
+            width: 40%;
+            /* To see the outline of the form */
+            padding: 1em;
+            border: 1px solid #CCC;
+            border-radius: 1em;
+        }
+
+        form div + div {
+            margin-top: 1em;
+        }
+
+        label {
+            /* To make sure that all labels have the same size and are properly aligned */
+            display: inline-block;
+            width: 30%;
+            text-align: right;
+        }
+
+        input, textarea {
+            /* To make sure that all text fields have the same font settings
+               By default, textareas have a monospace font */
+            font: 1em sans-serif;
+
+            /* To give the same size to all text fields */
+            width: 65%;
+            box-sizing: border-box;
+
+            /* To harmonize the look & feel of text field border */
+            border: 1px solid #999;
+        }
+
+        input:focus, textarea:focus {
+            /* To give a little highlight on active elements */
+            border-color: #000;
+        }
+
+        textarea {
+            /* To properly align multiline text fields with their labels */
+            vertical-align: top;
+
+            /* To give enough room to type some text */
+            height: 5em;
+        }
+
+        .button {
+            /* To position the buttons to the same position of the text fields */
+            padding-left: 90px; /* same size as the label elements */
+        }
+
+        button {
+            /* This extra margin represent roughly the same space as the space
+               between the labels and their text fields */
+            margin-left: .5em;
+        }
+
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-primary bg-primary">
