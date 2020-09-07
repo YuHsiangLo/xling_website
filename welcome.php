bc84e34d8f7e12add471a78a750d5c52cb934531
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-09-06 20:41:30 -0700

    Remove audio-related stuff from welcom.php

--- welcome.php
+++ welcome.php
@@ -26,40 +26,6 @@ require_once 'language/'.$lang.'/main.php';
     </style>
     <script src="/bootstrap/js/bootstrap.min.js"></script>
     <script src="/bootstrap/js/jquery.min.js"></script>
-    <script>
-        function initAudio() {
-            // Older browsers might not implement mediaDevices at all, so we set an empty object first
-            if (navigator.mediaDevices === undefined) {
-                navigator.mediaDevices = {};
-            }
-
-            // Some browsers partially implement mediaDevices. We can't just assign an object
-            // with getUserMedia as it would overwrite existing properties.
-            // Here, we will just add the getUserMedia property if it's missing.
-
-            if (navigator.mediaDevices.getUserMedia === undefined) {
-                navigator.mediaDevices.getUserMedia = function(constraints) {
-
-                    // First get ahold of the legacy getUserMedia, if present
-                    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-                    // Some browsers just don't implement it - return a rejected promise with an error
-                    // to keep a consistent interface
-                    if (!getUserMedia) {
-                        window.alert("Sorry, your web browser does not seem to support Web Audio API. The online recorder will not function. Please try upgrading your browser, or try a different browser.");
-                        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-                    }
-
-                    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-                    return new Promise(function(resolve, reject) {
-                        getUserMedia.call(navigator, constraints, resolve, reject);
-                    });
-                }
-            }
-        }
-
-        window.addEventListener('load', initAudio);
-    </script>
 </head>
 <body>
 <nav class="navbar navbar-primary bg-primary">
